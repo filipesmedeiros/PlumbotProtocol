59e53b4e6798939a6c74350b47560a4377b1f694
filipesilvamedeiros@gmail.com <CarbonSix6>
2018-11-23 00:24:38 +0000

    Blah blah blah

---
+++ src/message/xbot/OptimizationReplyMessage.java
@@ -0,0 +1,44 @@
+package message.xbot;
+
+import message.ControlMessage;
+
+import java.net.InetSocketAddress;
+import java.nio.ByteBuffer;
+
+public class OptimizationReplyMessage extends ControlMessage {
+
+    public final static short TYPE = 8;
+
+    private boolean accept;
+
+    public OptimizationReplyMessage(InetSocketAddress sender, boolean accept) {
+        super(sender, TYPE);
+        this.accept = accept;
+    }
+
+    @Override
+    public ByteBuffer bytes() {
+        ByteBuffer buffer = putSenderInBuffer();
+
+        buffer.put(accept ? (byte) 1 : (byte) 0);
+
+        buffer.put(EOT);
+
+        buffer.flip();
+
+        return buffer;
+    }
+
+    @Override
+    public short messageType() {
+        return TYPE;
+    }
+
+    public static OptimizationReplyMessage parse(ByteBuffer bytes) {
+        InetSocketAddress sender = parseAddress(bytes);
+
+        boolean accept = bytes.get() == 1;
+
+        return new OptimizationReplyMessage(sender, accept);
+    }
+}
